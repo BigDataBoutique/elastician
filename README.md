d7dee2cb1d9b67155b2928849234802b27d76b94
Itamar Syn-Hershko <itamar@bigdataboutique.com>
2020-12-01 11:59:47 +0000

    Feat elastic6

--- README.md
+++ README.md
@@ -30,17 +30,55 @@ docker run  --net=host -v `pwd`:/data --rm bigdataboutique/elastician dump myind
 docker run  --net=host -v `pwd`:/data --rm bigdataboutique/elastician ingest myindex_dump.jsonl.gz myindex --hosts http://10.63.246.27:9200
 ```
 
+Both dump and ingest now have additional parameters to allow accessing ES using a certificate.
+Make sure the certificate file has the appropriate file permissions.
+
+`--crtfile` - name of the certificate file. Notice that unless you specify a folder this should sit in the working directory of the container, which is /data .
+
+`--verify-cert/--no-verify-cert` - Whether you're using a signed cert and want to verify it. By default false. 
+
+
 ### Copy index between clusters
 
 ```bash
-docker run  --net=host --rm bigdataboutique/elastician copy source_index_name http://target-cluster:9200 --hosts http://10.63.246.27:9200
+docker run  --net=host --rm bigdataboutique/elastician copy source_index_name --target http://target-cluster:9200 --source http://10.63.246.27:9200
 ```
-### Copy more than one index between clusters
+### Perform a set of instructions on clusters (e.g. to copy indices between clusters)
 ```bash
-docker run  --net=host -v `pwd`:/instructions --rm bigdataboutique/elastician copy-cluster /instructions/source.csv /instructions/target.csv  http://target-cluster:9200 --hosts http://10.63.246.27:9200
+docker run  --net=host -v `pwd`:/instructions --rm bigdataboutique/elastician copy-cluster /instructions/source.csv /instructions/target.csv  --target http://target-cluster:9200 --source http://10.63.246.27:9200
 ```
 First file name is csv with rows in the following structure:
 ```
-index_name,instruction,to_del
+instruction,index_name,to_del
 ```
-where instruction is either `copy` or `dump`, to_del is `X` if the index in the source is to be deleted following dump or copy
+where instruction is either `copy`, `dump`, `delete`, to_del is `X` if the index in the source is to be deleted following the action
+
+For `ingest` the format is:
+```
+ingest,file_name,index_name
+```
+Other parameters for copy-cluster:
+
+`--abort-on-failure`: stops the default is true
+
+`--dump-timeout` - default is u'1m'
+
+`--delete-timeout` - default is 60
+
+`--error-on-timeout`: whether the delete function should return an error on timeout
+
+`--preserve-index/--no-preserve-index` - whether the index saved in the dumps should be used during ingest
+
+Copy and copy-cluster also have certificate parameters, that work similarly to those above:
+
+`--crtfile-target`
+
+`--verify-cert-target/--no-verify-cert`
+
+`--crtfile-source`
+
+`--verify-cert-source/--no-verify-cert-source`
+
+Ingest, Copy and copy cluster have a transformations parameter, supporting a comma-separated list
+of transformations. Currently available transformations:
+`boolean_lowercase` - turn True and False into 'true' and 'false'
--- elastician/tools.py
+++ elastician/tools.py
@@ -8,6 +8,11 @@ import elasticsearch
 from tqdm import tqdm
 from elasticsearch import Elasticsearch, helpers
 import csv
+import urllib3.exceptions
+
+from ssl import create_default_context
+from ssl import CERT_NONE
+import urllib3
 
 logging.basicConfig()
 logger = logging.getLogger()
@@ -17,25 +22,82 @@ logger = logging.getLogger()
 def cli():
     pass
 
+### dictionary and JSON helper functions
+def read_json_from_gzip_file(f):
+    for line in f:
+        yield json.loads(line.decode(encoding='UTF-8'))
 
+
+def nested_replace( structure, transform_map ):
+    if type(structure) == list:
+        return [nested_replace( item, transform_map) for item in structure]
+
+    if type(structure) == dict:
+        return {key : nested_replace(value, transform_map)
+                     for key, value in structure.items() }
+
+    if structure in transform_map.keys():
+        return transform_map[structure]
+    else:
+        return structure
+
+def apply_transformations(doc,trans_list):
+    transform_map = {}
+    if 'boolean_lowercase' in trans_list:
+        transform_map.update({'True':'true','False':'false'})
+    if transform_map:
+        return nested_replace(doc,transform_map)
+    else:
+        return doc
+
+
+### Elasticsearch helper functions
 def get_es_hosts(hosts):
     es_hosts = hosts or os.getenv('ES_HOSTS') or 'localhost:9200'
     return es_hosts.split(',')
 
+def get_es(hosts,crtfile,verify_cert):
+    if crtfile is not None:
+        context = create_default_context(cafile=crtfile)
+        if not verify_cert:
+            context.check_hostname = False
+            context.verify_mode = CERT_NONE
+            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        return Elasticsearch(hosts=get_es_hosts(hosts),ssl_context=context)
+    return Elasticsearch(hosts=get_es_hosts(hosts))
 
-def delete_func(index, es_source):
+def get_target_type(es):
+    version = (int)(es.info()['version']['number'][0])
+    if version >= 7:
+        type = None
+    else:
+        type = "_doc"
+    return type
+
+
+### interface functions and main implementation
+def delete_func(index, es_source,timeout,error_on_timeout):
     try:
-        es_source.indices.delete(index)
+        es_source.indices.delete(index,request_timeout=timeout)
     except elasticsearch.exceptions.NotFoundError:
+        #TODO wildcards never get here even if no index exists. think whether implementing a check
         click.echo(f'Error deleting index {index}: Not Found', err=True)
+        return False
+    except (elasticsearch.exceptions.ConnectionTimeout, urllib3.exceptions.ReadTimeoutError):
+        click.echo(f'Error deleting index {index}: Timeout', err=True)
+        return error_on_timeout
+    return True
+
 
 
 @cli.command()
 @click.argument('index')
 @click.option('--hosts')
 @click.option('--timeout', default=u'1m')
-def dump(index, hosts, timeout):
-    es_source = Elasticsearch(hosts=get_es_hosts(hosts))
+@click.option('--crtfile')
+@click.option('--verify-cert/--no-verify-cert', default=False)
+def dump(index, hosts, timeout,crtfile,verify_cert):
+    es_source = get_es(hosts,crtfile,verify_cert)
     dump_func(index, es_source, timeout)
 
 
@@ -59,53 +121,90 @@ def dump_func(index, es_source, timeout):
 @cli.command()
 @click.argument('in_filename')
 @click.argument('out_filename')
-@click.argument('target')
-@click.option('--hosts')
-@click.option('--timeout', default=u'1m')
-def copy_cluster(in_filename, out_filename, target, hosts, timeout):
-    es_source = Elasticsearch(hosts=get_es_hosts(hosts))
-    es_target = Elasticsearch(hosts=get_es_hosts(target))
+@click.option('--target')
+@click.option('--source')
+@click.option('--delete_timeout', default=60)
+@click.option('--error_on_timeout', default=False)
+@click.option('--preserve-index/--no-preserve-index', default=True)
+@click.option('--preserve-ids/--no-preserve-ids', default=False)
+@click.option('--abort-on-failure', default=True)
+@click.option('--dump_timeout', default=u'1m')
+@click.option('--crtfile-target')
+@click.option('--verify-cert-target/--no-verify-cert-target', default=False)
+@click.option('--crtfile-source')
+@click.option('--verify-cert-source/--no-verify-cert-source', default=False)
+@click.option('--transformations')
+def copy_cluster(in_filename, out_filename, target, source,delete_timeout,error_on_timeout,preserve_index,preserve_ids,
+                 abort_on_failure,dump_timeout,crtfile_target,verify_cert_target,crtfile_source,verify_cert_source,transformations):
+    if target is None and source is None:
+        click.echo(f'No relevant Elasticsearch instances', err=True)
+        return
+    es_source = get_es(source, crtfile_source, verify_cert_source)
+    es_target = get_es(target, crtfile_target, verify_cert_target)
+    trans_list = transformations.split(",")
+    #TODO override ES's behavior to use localhost as a default
     with open(out_filename, 'w') as out_file, open(in_filename, newline='') as in_file:
         reader = csv.reader(in_file, delimiter=',', quotechar='|')
         writer = csv.writer(out_file)
         for row in reader:
-            if len(row) == 2:
-                cur_index, cur_op = row
-                to_del = ""
-            elif len(row) == 3:
-                cur_index, cur_op, to_del = row
+            cur_op = row[0]
+            to_del = ""
             ok = False
-            if cur_op == "copy":
-                ok = copy_func(cur_index, es_target, es_source)
-            elif cur_op == "dump":
-                ok = dump_func(cur_index, es_source, timeout)
+            if cur_op == "copy" or cur_op == "dump":
+                cur_index = row[1]
+                if len(row) == 3:
+                    to_del = row[2]
+                if cur_op == "copy":
+                    ok = copy_func(cur_index, es_target, es_source,trans_list)
+                elif cur_op == "dump":
+                    ok = dump_func(cur_index, es_source,dump_timeout)
+            elif cur_op == "delete":
+                cur_index = row[1]
+                ok = delete_func(cur_index, es_source,delete_timeout,error_on_timeout)
+            elif cur_op == "ingest":
+                cur_file = row[1]
+                cur_index = None
+                if len(row) == 3:
+                    cur_index = row[2]
+                ok = ingest_func(cur_file,cur_index,es_target,preserve_index,preserve_ids,trans_list)
+            result_row = row
             if ok is False:
-                return
-            if to_del == "X":
-                delete_func(cur_index, es_source)
-            writer.writerow(row)
+                if abort_on_failure is True:
+                    return
+                result_row.append("failed")
+            if to_del == "X" and cur_op != "delete" and ok is True:
+                ok = delete_func(cur_index, es_source,delete_timeout,error_on_timeout)
+                if not ok:
+                    result_row.append("delete failed")
+            writer.writerow(result_row)
 
 
 @cli.command()
 @click.argument('index')
-@click.argument('target')
-@click.option('--hosts')
-def copy(index, target, hosts):
-    es_source = Elasticsearch(hosts=get_es_hosts(hosts))
-    es_target = Elasticsearch(hosts=get_es_hosts(target))
-    copy_func(index, es_target, es_source)
+@click.option('--target')
+@click.option('--source')
+@click.option('--crtfile-target')
+@click.option('--verify-cert-target/--no-verify-cert', default=False)
+@click.option('--crtfile-source')
+@click.option('--verify-cert-source/--no-verify-cert-source', default=False)
+@click.option('--transformations')
+def copy(index, target, source,crtfile_target,verify_cert_target,crtfile_source,verify_cert_source,transformations):
+    es_source = get_es(source, crtfile_source, verify_cert_source)
+    es_target = get_es(target, crtfile_target, verify_cert_target)
+    trans_list = transformations.split(",")
+    copy_func(index, es_target, es_source,trans_list)
 
 
-def copy_func(index, es_target, es_source):
+def copy_func(index, es_target, es_source,trans_list):
     docs = helpers.scan(es_source, index=index,
                         query={"sort": ["_doc"]},
                         scroll=u'1m', raise_on_error=True, preserve_order=False)
-
+    type = get_target_type(es_target)
     indexer = helpers.streaming_bulk(es_target, (dict(
         _index=doc['_index'],
-        _type='_doc',
+        _type=type,
         _op_type="index",
-        **doc['_source']) for doc in docs))
+        **(apply_transformations(doc['_source'],trans_list))) for doc in docs))
     try:
         for _ in tqdm(indexer):
             pass
@@ -121,16 +220,25 @@ def copy_func(index, es_target, es_source):
 @click.option('--hosts')
 @click.option('--preserve-index/--no-preserve-index', default=True)
 @click.option('--preserve-ids/--no-preserve-ids', default=False)
-def ingest(path, index, hosts, preserve_index, preserve_ids):
-    es = Elasticsearch(hosts=get_es_hosts(hosts))
+@click.option('--crtfile')
+@click.option('--verify-cert/--no-verify-cert', default=False)
+@click.option('--transformations')
+def ingest(path, index, hosts, preserve_index, preserve_ids,crtfile,verify_cert,transformations):
+    es_target = get_es(hosts,crtfile,verify_cert)
+    trans_list = transformations.split(",")
+    ingest_func(path, index, es_target, preserve_index, preserve_ids,trans_list)
+
+
+def ingest_func(path, index, es_target, preserve_index, preserve_ids,trans_list):
     with gzip.open(path, mode='rb') as f:
-        objs = [json.loads(line.decode(encoding='UTF-8')) for line in f]
-        it = helpers.streaming_bulk(es, (dict(
+        type = get_target_type(es_target)
+        #objs = [json.loads(line.decode(encoding='UTF-8')) for line in f]
+        it = helpers.streaming_bulk(es_target, (dict(
             _index=index if not preserve_index else o['_index'],
-            _type='_doc',
+            _type=type,
             _id=None if not preserve_ids else o['_id'],
             _op_type="index",
-            **(o['_source'])) for o in objs))
+            **(apply_transformations(o['_source'],trans_list))) for o in read_json_from_gzip_file(f)),max_chunk_bytes=10*1024*1024)
         for ok, response in it:
             if not ok:
                 click.echo(f'Error indexing to {index}: response is {response}', err=True)
